1996c0fa39ddc62a9453eca03af6c456025a0251
WakingHours-GitHub <WakingHoursHUC@outlook.com>
2022-03-21 18:17:10 +0800

    3.21

--- study/qianfeng/1_dome01/entry/src/main/java/com/example/dome01/MainAbility.java
+++ study/qianfeng/1_dome01/entry/src/main/java/com/example/dome01/MainAbility.java
@@ -3,6 +3,9 @@ package com.example.dome01;
 import com.example.dome01.slice.MainAbilitySlice;
 import ohos.aafwk.ability.Ability;
 import ohos.aafwk.content.Intent;
+import ohos.agp.render.render3d.Task;
+import ohos.app.dispatcher.TaskDispatcher;
+import ohos.app.dispatcher.task.TaskPriority;
 
 /*
 启动流程
@@ -13,5 +16,33 @@ public class MainAbility extends Ability {
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setMainRoute(MainAbilitySlice.class.getName());
+
+        // 疯狂匿名实现Thread并且直接开启线程.
+        new Thread( // .start直接开启
+                new Runnable() { // 重载接口中的方法
+                    @Override
+                    public void run() {
+
+                    }
+                }
+        ).start();
+
+        /*
+        鸿蒙的方式:
+        // 获取全局任务分配程序
+        TaskDispatcher taskDispatcher = getGlobalTaskDispatcher(TaskPriority.DEFAULT);
+        taskDispatcher.asyncDispatch(
+                new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                }
+        );*/
+
+
     }
+
 }
+
+
